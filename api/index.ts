2a68017e124070803798a93c12b083da438b844a
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2023-04-05 23:32:52 +0200

    Log as added

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import LogDispatcher from "./lib/logDispatcher";
-
-const log = LogDispatcher.getInstance(true, "info");
 dotenv.config();
+import { Logger, LogLevel } from "./lib/logger";
+const logger = Logger.getInstance(true, process.env.LOGLEVEL as LogLevel);
 
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Custodian");
+  res.send("Custodian is online");
 });
 
 app.listen(port, () => {
-  log.console(`Service is live on ${port}.`);
+  logger.info(`Service is live on ${port}.`);
+  logger.error(`Service is live on ${port}.`);
+  logger.warn(`Service is live on ${port}.`);
 });
--- api/out/index.js
+++ api/out/index.js
@@ -5,15 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const logDispatcher_1 = __importDefault(require("./lib/logDispatcher"));
-const log = logDispatcher_1.default.getInstance(true, "info");
 dotenv_1.default.config();
+const logger_1 = require("./lib/logger");
+const logger = logger_1.Logger.getInstance(true, process.env.LOGLEVEL);
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get("/", (req, res) => {
-    res.send("Custodian");
+    res.send("Custodian is online");
 });
 app.listen(port, () => {
-    log.console(`Service is live on ${port}.`);
+    logger.info(`Service is live on ${port}.`);
+    logger.error(`Service is live on ${port}.`);
+    logger.warn(`Service is live on ${port}.`);
 });
 //# sourceMappingURL=index.js.map
