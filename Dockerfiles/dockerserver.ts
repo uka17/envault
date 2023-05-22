a9e4d285e739a177ade43e99b2e30a5cf5ea286e
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2023-05-22 23:15:57 +0200

    Removed keys

---
+++ Dockerfiles/dockerserver.ts
@@ -0,0 +1,21 @@
+//Run docker build in this script in order to avoid expousing or ENV variables with keys
+import dotenv from "dotenv";
+import chalk from "chalk";
+dotenv.config();
+const { exec } = require("child_process");
+
+const com = `docker build --tag uka17/custodian-server:latest . -f Dockerfiles/server --build-arg JWT_SECRET="${process.env.JWT_SECRET}" --build-arg DB="${process.env.DB}" --build-arg ENV="${process.env.ENV}" --build-arg MAILAPI="${process.env.MAILAPI}" && docker push uka17/custodian-api:latest`;
+
+console.log(`Executing: ${chalk.green(com)}...`);
+
+exec(com, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
