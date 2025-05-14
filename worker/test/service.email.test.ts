fc455c81dfae9401c78d820e11b0e2681a72b29e
Nikolai Kozhekhov <ukaoneseven@gmail.com>
2025-05-14 21:20:51 +0200

    Adds some tests

---
+++ worker/test/service.email.test.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+
+describe("Service tests", () => {
+  before(async() => {
+
+  });
+
+  after(async() => {});
+
+  describe("Example test", () => {
+    it("should do a simple test", async() => {
+      expect(true).to.equal(true);
+    });
+  });
+});
